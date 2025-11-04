caeb29d613e319d1b4462124444876462af5737a
stefanstudiodirection <stefan.radovic@studiodirection.com>
2025-11-04 13:16:05 +0100

    upload font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  fontFamily: {
+    sans: ['Founders Grotesk', 'sans-serif'],
+  },
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
